2dee485613727241a807726838df1e4ac0323abe
Junanjunan <taltalrealty@gmail.com>
2024-11-12 18:38:46 +0900

    [add] My own keymap

--- examples/keyboard/components/esp32_s3_kbd_kit/include/bsp/keyboard.h
+++ examples/keyboard/components/esp32_s3_kbd_kit/include/bsp/keyboard.h
@@ -15,35 +15,37 @@ extern "C" {
 
 /*!< Keyboard matrix GPIO */
 #define KBD_ROW_NUM  6
-#define KBD_COL_NUM  15
+#define KBD_COL_NUM  17
 
-#define KBD_ROW_IO_0 40
-#define KBD_ROW_IO_1 39
-#define KBD_ROW_IO_2 38
-#define KBD_ROW_IO_3 45
-#define KBD_ROW_IO_4 48
-#define KBD_ROW_IO_5 47
+#define KBD_ROW_IO_0 4
+#define KBD_ROW_IO_1 5
+#define KBD_ROW_IO_2 6
+#define KBD_ROW_IO_3 7
+#define KBD_ROW_IO_4 15
+#define KBD_ROW_IO_5 16
 
-#define KBD_COL_IO_0 21
-#define KBD_COL_IO_1 14
-#define KBD_COL_IO_2 13
-#define KBD_COL_IO_3 12
-#define KBD_COL_IO_4 11
-#define KBD_COL_IO_5 10
-#define KBD_COL_IO_6 9
-#define KBD_COL_IO_7 4
-#define KBD_COL_IO_8 5
-#define KBD_COL_IO_9 6
-#define KBD_COL_IO_10 7
-#define KBD_COL_IO_11 17
-#define KBD_COL_IO_12 3
-#define KBD_COL_IO_13 18
-#define KBD_COL_IO_14 8
+#define KBD_COL_IO_0 1
+#define KBD_COL_IO_1 2
+#define KBD_COL_IO_2 42
+#define KBD_COL_IO_3 41
+#define KBD_COL_IO_4 40
+#define KBD_COL_IO_5 39
+#define KBD_COL_IO_6 38
+#define KBD_COL_IO_7 37
+#define KBD_COL_IO_8 36
+#define KBD_COL_IO_9 35
+#define KBD_COL_IO_10 45
+#define KBD_COL_IO_11 48
+#define KBD_COL_IO_12 47
+#define KBD_COL_IO_13 21
+#define KBD_COL_IO_14 14
+#define KBD_COL_IO_15 13
+#define KBD_COL_IO_16 12
 
 #define KBD_OUTPUT_IOS {KBD_ROW_IO_0, KBD_ROW_IO_1, KBD_ROW_IO_2, KBD_ROW_IO_3, KBD_ROW_IO_4, KBD_ROW_IO_5}
 #define KBD_INPUT_IOS  {KBD_COL_IO_0, KBD_COL_IO_1, KBD_COL_IO_2, KBD_COL_IO_3, KBD_COL_IO_4, KBD_COL_IO_5, \
                         KBD_COL_IO_6, KBD_COL_IO_7, KBD_COL_IO_8, KBD_COL_IO_9, KBD_COL_IO_10, KBD_COL_IO_11,\
-                        KBD_COL_IO_12, KBD_COL_IO_13, KBD_COL_IO_14}
+                        KBD_COL_IO_12, KBD_COL_IO_13, KBD_COL_IO_14, KBD_COL_IO_15, KBD_COL_IO_16}
 
 /*!< WS2812 power control GPIO */
 #define KBD_WS2812_POWER_IO  1
